099b7966884a70162b71a085b949ee992563f881
roshansu <163381434+roshansu@users.noreply.github.com>
2025-04-12 16:47:17 +0530

    Update Store.js

--- src/Store.js
+++ src/Store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import inputState from "../../Frontend/src/Slice";
+import inputState from "./Slice";
 
 const store = configureStore({
     reducer: {
