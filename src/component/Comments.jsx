c8cd45ae357fc6b09e3cb444e55d186e87190d72
roshansu <163381434+roshansu@users.noreply.github.com>
2025-04-13 09:34:13 +0530

    Update Comments.jsx

--- src/component/Comments.jsx
+++ src/component/Comments.jsx
@@ -5,7 +5,7 @@ const Comments = ({cData}) => {
     <div className='mt-3 border-b-2 pb-3 border-amber-700'>
        <div className='flex  text-gray-700 text-lg font-medium items-center gap-2'>
                 <i className="fa-solid fa-circle-user"></i>
-                <p>&bull; Anonymous</p>
+                <p>&bull; unknown user</p>
         </div>
         <p className='text-gray-600 mt-1 text-lg'>{cData}</p>
     </div>
