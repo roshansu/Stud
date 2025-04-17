1b49984866d031fa680ce34e96acee87e2524d25
roshansu <163381434+roshansu@users.noreply.github.com>
2025-04-17 14:43:10 +0530

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -80,14 +80,14 @@ const Home = () => {
 
     if(loading) return <div className='flex px-2 justify-center flex-col items-center h-screen'>
         <i className="fa-solid  fa-spinner animate-spin text-4xl"></i>
-        <p className='lg:text-2xl lg:w-[70%] text-xl mt-3 text-center'>i am back</p>
+        <p className='lg:text-2xl lg:w-[70%] text-xl mt-3 text-center'>If the site is still loading so plz connect the wifi then open it</p>
     </div>
 
 
   return (
     <div className='bg-[#eef4ed] pb-16 pt-14  lg:pt-24'>
        <div className='flex  justify-center'>
-            <p className='lg:max-w-1/2 p-2 text-[#023047] text-lg font-bold  text-center lg:text-2xl'> you are not supposed to comment only you can see comments on other's profile
+            <p className='lg:max-w-1/2 p-2 text-[#023047] text-lg font-bold  text-center lg:text-2xl'> you cannot comment only you can see comments on other's profile
         </p>
        </div>
        <div className='flex mt-6 lg:flex-nowrap flex-wrap px-2 justify-center items-center gap-3 lg:text-base text-sm lg:gap-6'>
